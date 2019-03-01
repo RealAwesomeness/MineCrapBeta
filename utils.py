6376b6392011895f4af9dc19d4f9b3fd2a1e24f9
RealAwesomeness <40503841+RealAwesomeness@users.noreply.github.com>
2019-03-01 08:34:22 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import sys
 import os
 def startminer(miner, start, algo, config) :
 	start = start.replace("ALGO", algo)
-	start = start.replace("ADDRESS", config[algo]["address"])
+	start = start.replace("ADDRESS", config["addresses"]["bitcoin"])
 	start = start.replace("POOL", config[algo]["pool"])
 	start = start.replace("PORT", config[algo]["port"]
 	if sys.platform.startswith("win"):

ad674e08909300be151dca49d2e7c44455b27884
RealAwesomeness <40503841+RealAwesomeness@users.noreply.github.com>
2019-03-01 11:20:24 -0500

    finished create config and preemptively fix stuff mentioned earlier

--- utils.py
+++ utils.py
@@ -6,16 +6,17 @@ def startminer(miner, start, algo, config):
 	start = start.replace("ADDRESS", config["addresses"]["bitcoin"])
 	found = False
 	for pool in config["pools"]:
-		if algo in pool["algorithms"] and not Found: #uses first pool with selected algorithm supported
-			if pool["algorithms"][algo]["subdomain"]:
-				start = start.replace("POOL", pool["url"])
-				start = start.replace("SUBDOMAIN", pool["algorithms"][algo]["subdomain"] + ".")
-				start = start.replace("PORT", pool["algorithms"][algo]["port"])
+		temppath = config["pools"][pool]
+		if algo in temppath["algorithms"] and not Found: #uses first pool with selected algorithm supported
+			if temppath["algorithms"][algo]["subdomain"]:
+				start = start.replace("POOL", temppath["url"])
+				start = start.replace("SUBDOMAIN", temppath["algorithms"][algo]["subdomain"] + ".")
+				start = start.replace("PORT", temppath["algorithms"][algo]["port"])
 				found = True
 			else:
-				start = start.replace("POOL", pool["url"])
+				start = start.replace("POOL", temppath["url"])
 				start = start.replace("SUBDOMAIN", "")
-				start = start.replace("PORT", pool[algo]["port"])
+				start = start.replace("PORT", temppath[algo]["port"])
 				found = True
 	if sys.platform.startswith("win"):
 		start = miner + "\\" + miner + ".exe" + start
@@ -28,3 +29,15 @@ def createconfig():
 	config = json.load(configexample.read())
 	for address in addresses:
 		addresses[address] = input("What is your " + str(address) + " address? ")
+	getOut = False
+	while not getOut:
+		newpool = input("Enter a pool URL (Don't enter the subdomain!)")
+		config["pools"][newpool]["url"] = newpool
+		algo = "ipsum lorem"
+		while not algo:
+			algo = input("Enter an algo this pool supports. If there aren't anymore just press enter : ")
+			subdomain = input("Enter the subdomain this is located on if applicable (Press enter if it's not). If you don't know what that is, search it up : ")
+			port = input("Enter the port for this algo : ")
+			if not algo:
+				config["pools"][newpool]["algorithms"][algo]["port"] = port
+				config["pools"][newpool]["algorithms"][algo]["subdomain"] = subdomain + "."

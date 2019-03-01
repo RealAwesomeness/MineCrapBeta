60ed51ff9efbd101dc42fce825760debab0754c4
RealAwesomeness <40503841+RealAwesomeness@users.noreply.github.com>
2019-03-01 08:55:40 -0500

    make startminer use this terrible config

--- utils.py
+++ utils.py
@@ -3,8 +3,19 @@ import os
 def startminer(miner, start, algo, config) :
 	start = start.replace("ALGO", algo)
 	start = start.replace("ADDRESS", config["addresses"]["bitcoin"])
-	start = start.replace("POOL", config[algo]["pool"])
-	start = start.replace("PORT", config[algo]["port"]
+	found = False
+	for pool in config["pools"]:
+		if algo in pool["algorithms"] and not Found: #uses first pool with selected algorithm supported
+			if pool["algorithms"][algo]["subdomain"]:
+				start = start.replace("POOL", pool["url"])
+				start = start.replace("SUBDOMAIN", pool["algorithms"][algo]["subdomain"] + ".")
+				start = start.replace("PORT", pool["algorithms"][algo]["port"])
+				found = True
+			else:
+				start = start.replace("POOL", pool["url"])
+				start = start.replace("SUBDOMAIN", "")
+				start = start.replace("PORT", pool[algo]["port"])
+				found = True
 	if sys.platform.startswith("win"):
 		start = miner + "\\" + miner + ".exe" + start
 	else:
